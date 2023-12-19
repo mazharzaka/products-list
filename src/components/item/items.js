1963d2b897b94605e63cd8e041735057ccf530e7
mazharzaka <mazharmohamed566@gmail.com>
2023-12-19 15:07:09 +0200

    changes

--- src/components/item/items.js
+++ src/components/item/items.js
@@ -4,24 +4,48 @@ const Items = (props) => {
   const {items, del, Qty} = props;
   //   const {value, setValue} = 1;
   let length = items.length;
+  const min = (e, id) => {
+    let value = document.querySelector("#count" + id).value;
 
+    if (value !== "0") {
+      document.querySelector("#count" + id).value = Number(value) - 1;
+      value = Number(document.querySelector("#count" + id).value);
+      Qty(id, value);
+    }
+
+    // console.log(e.target.parentElement.children[1].value);
+  };
+  const max = (e, id) => {
+    let value = document.querySelector("#count" + id).value;
+    // console.log(value);
+    if (value !== "10") {
+      document.querySelector("#count" + id).value = Number(value) + 1;
+      value = Number(document.querySelector("#count" + id).value);
+      Qty(id, value);
+    }
+    // console.log(e.target.parentElement.children[1].value);
+    console.log(value);
+  };
   const ListItem = length ? (
     items.map((item) => {
       return (
         <div key={item.id} className="item">
           <p>{item.product}</p>
           <p>
-            <input
-              className="count"
-              defaultValue={item.qty}
-              onChange={(e) => {
-                let num = Number(e.target.value);
-                Qty(item.id, num);
-                console.log(item.qty);
-              }}
-              type="number"
-              min={0}
-            />
+            <div class="number">
+              <span onClick={(e) => min(e, item.id)} class="minus">
+                -
+              </span>
+              <input
+                className="count"
+                value={item.qty}
+                type="text"
+                id={"count" + item.id}
+              />
+              <span class="plus" onClick={(e) => max(e, item.id)}>
+                +
+              </span>
+            </div>
           </p>
           <p>{item.price * item.qty}</p>
           <p className="delete" onClick={() => del(item.id)}>
