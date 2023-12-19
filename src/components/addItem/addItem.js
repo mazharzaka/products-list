4ed82e5a2403d887e96f53d1ac077171338b4801
mazharzaka <mazharmohamed566@gmail.com>
2023-12-19 14:12:54 +0200

    task 2

--- src/components/addItem/addItem.js
+++ src/components/addItem/addItem.js
@@ -1,38 +1,65 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
 
 class addItem extends Component {
-    state = {
-        product: '',
-        price: ''
-    }
+  state = {
+    product: "",
+    qty: "",
+    price: "",
+  };
 
-    handleChange = (e) => {
-        console.log(e.target.id + ": " + e.target.value)
-        this.setState({
-            [e.target.id]: e.target.value
-        })
-    }
+  handleChange = (e) => {
+    console.log(e.target.id + ": " + e.target.value);
+    // let qty= document.getElementById('qty')
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
 
-    handleSubmit = (e) => {
-        e.preventDefault()
-        this.props.add(this.state)
-        this.setState({
-            product: '',
-            price: ''
-        })
-    }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // this.state.price = Number(this.state.price) * Number(this.state.qty);
+    // console.log(this.state);
+    this.props.add(this.state);
+    this.setState({
+      product: "",
+      qty: "",
+      price: "",
+    });
+  };
 
-    render(){
-        return(
-            <div className="item">
-                <form onSubmit={this.handleSubmit}>
-                    <input type="text" value={this.state.product} placeholder="Enter Product" id="product" onChange={this.handleChange} required/>
-                    <input type="number" value={this.state.price} placeholder="Enter Price" id="price" onChange={this.handleChange} required/>
-                    <input type="submit" value="Add"/>
-                </form>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="item">
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.product}
+            placeholder="Enter Product"
+            id="product"
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            type="number"
+            value={this.state.qty}
+            placeholder="Enter Quantity"
+            id="qty"
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            type="number"
+            value={this.state.price}
+            placeholder="Enter Price"
+            id="price"
+            onChange={this.handleChange}
+            required
+          />
+          <input type="submit" value="Add" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default addItem;
--- src/components/item/items.js
+++ src/components/item/items.js
@@ -1,31 +1,49 @@
-import React from 'react';
+import React from "react";
 
 const Items = (props) => {
-    const {items, del} = props;
-    let length = items.length
-    const ListItem = length ? (
-        items.map(item => {
-            return(
-                <div key={item.id} className="item">
-                    <p>{item.product}</p>
-                    <p>{item.price}</p>
-                    <p className="delete" onClick={() => del(item.id)}>&times;</p>
-                </div>
-            )
-        })
-    ) : (
-        <div className="text">There are no items, Try to add some.</div>
-    )
-    return (
-        <div>
-            <div className="header item">
-                <p>Product</p>
-                <p>Price</p>
-                <p>Edit</p>
-            </div>
-            {ListItem}
-        </div>
-    )
-}
+  const {items, del, Qty} = props;
+  //   const {value, setValue} = 1;
+  let length = items.length;
 
-export default Items
+  const ListItem = length ? (
+    items.map((item) => {
+      return (
+        <div key={item.id} className="item">
+          <p>{item.product}</p>
+          <p>
+            <input
+              className="count"
+              defaultValue={item.qty}
+              onChange={(e) => {
+                let num = Number(e.target.value);
+                Qty(item.id, num);
+                console.log(item.qty);
+              }}
+              type="number"
+              min={0}
+            />
+          </p>
+          <p>{item.price * item.qty}</p>
+          <p className="delete" onClick={() => del(item.id)}>
+            &times;
+          </p>
+        </div>
+      );
+    })
+  ) : (
+    <div className="text">There are no items, Try to add some.</div>
+  );
+  return (
+    <div>
+      <div className="header item">
+        <p>Product</p>
+        <p>Quantity</p>
+        <p>Price</p>
+        <p>Edit</p>
+      </div>
+      {ListItem}
+    </div>
+  );
+};
+
+export default Items;
